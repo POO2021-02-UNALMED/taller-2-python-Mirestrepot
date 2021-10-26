3e78635fe4f571546b76b8b011d864f260da5701
Mirestrepot <92109447+Mirestrepot@users.noreply.github.com>
2021-10-25 21:05:02 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -2,61 +2,61 @@ from typing import List
 
 
 class Asiento:
-    def __init__(self, color, precio, registro):
+    def __init__(self, color:str, precio:int, registro:int) -> None:
         self.color = color
         self.precio = precio
         self.registro = registro
 
-    def cambiarColor(self, color):
+    def cambiarColor(self, color:str):
         colores = ["rojo", "verde", "amarillo", "negro", "blanco"]
         if color in colores:
             self.color = color
         else:
-                pass
+            pass
 
 
-
-
-class Auto:
-
-    cantidadCreados=0
-
-    def __init__(self, modelo, precio, asientos, marca, motor, registro):
-        self.modelo=modelo
-        self.asientos=asientos
-        self.marca=marca
-        self.motor=motor
-        self.registro=registro
-
-    def cantidadAsientos(self):
-
-        cont=0
-
-        for i in self.asientos:
-            if i!= None:
-                cont +=1
-        return cont
-
-
-    def verificarIntegridad(self):
-            if self.asientos[0].registro == self.registro:
-                if self.registro == self.motor.registro:
-                    return print("Auto original")
-                else:
-                    return print("Las piezas no son originales")
-            else:
-                return print("Las piezas no son originales")
-
-                
 class Motor:
-    def __init__(self, numeroCilindros, tipo, registro):
+    def __init__(self, numeroCilindros, tipo, registro) -> None:
         self.numeroCilindros = numeroCilindros
         self.tipo = tipo
         self.registro = registro
 
-    def cambiarRegistro(self,registro):
-        self.registro = registro
+    def cambiarRegistro(self,registros):
+        self.registro = registros
 
     def asignarTipo(self, tipo):
         if tipo=="electrico" or tipo=="normal":
             self.tipo=tipo
+
+
+class Auto:
+
+    cantidadCreados = 0
+
+    def __init__(self, modelo, precio, asientos: List, marca, motor:Motor,registro) -> None:
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+        Auto.cantidadCreados += 1
+
+    def cantidadAsientos(self):
+
+        contador=0
+
+        for i in self.asientos:
+            if i!= None:
+                contador+=1
+        return contador
+
+
+
+    def verificarIntegridad(self):
+        for i in self.asientos:
+            if i!= None:
+                if i.registro != self.registro or i.registro != self.motor.registro:
+                    return print("Las piezas no son originales")
+            
+        return print  ("Auto original")
